97c9b90395ba5547529073fed2e94b08261616c8
Yunseo Kim <s2102@e-mirim.hs.kr>
2023-04-20 19:41:07 +0900

    0420

--- ejssample/appejs.js
+++ ejssample/appejs.js
@@ -9,15 +9,35 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 
+// ejs 모듈 추가
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.get('/', (req, res) => {
+    
     console.log('/ get start ejs test');
-    fs.readFile('ejsex02.ejs', "utf-8", (err, data) => {
-        if(err) {
-            console.log('파일 읽기 에러 ' + err);
-        }else {
-            res.send(ejs.render(data));
-        }
+    res.render('ejsex03', {
+        dataArr: [
+            { name: '윤서', age: 19 },
+            { name: '신흥', age: 18 },
+            { name: '채영', age: 17 },
+            { name: '서영', age: 16 }
+        //     '0번 인덱스는 처음데이터',
+        //     '1번 인덱스는 둘째데이터',
+        //     '2번 인덱스는 세번째 데이터 입니다.'
+        ]
+        // jemok: 'ejs 제목 데이터',
+        // count: 4,
+        // hakbun: 3102,
+        // name: '김윤서'
     });
+    // fs.readFile('ejsex02.ejs', "utf-8", (err, data) => {
+    //     if(err) {
+    //         console.log('파일 읽기 에러 ' + err);
+    //     }else {
+    //         res.send(ejs.render(data));
+    //     }
+    // });
 });
 
 app.get('/login', (request, response) => {
